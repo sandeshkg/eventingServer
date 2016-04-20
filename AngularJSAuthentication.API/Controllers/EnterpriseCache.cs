a8f13b3c20f12d0e2358f6faeba0088134032f14
shancse <shanmugamcse@gmail.com>
2016-04-20 18:22:57 +0530

    Apr 20-2

---
+++ AngularJSAuthentication.API/Controllers/EnterpriseCache.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Practices.EnterpriseLibrary.Caching;
+
+namespace AngularJSAuthentication.API.Controllers
+{
+    public class EnterpriseCache
+    {
+
+        private static ICacheManager instance;
+
+        private EnterpriseCache() { }
+
+        public static ICacheManager Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = CacheFactory.GetCacheManager();
+                }
+                return instance;
+            }
+        }
+    }
+}
